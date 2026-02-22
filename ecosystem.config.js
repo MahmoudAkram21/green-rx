ba6b5e5fb8673f2dc79fcfaa01be2fcc385ab9c1
MahmoudAkram21 <mahmoudakram422@gmail.com>
2026-02-22 14:01:14 +0200

    last update

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports =  {
+  apps : [{
+    name      : 'green-rx-backend', // A unique name for your application
+    script    : 'dist/src/server.js',      // The entry point file for your Node.js app
+    instances : 'max',         // 'max' will use all available CPU cores for load balancing
+    exec_mode : 'cluster',     // Run in cluster mode
+    watch     : true,          // Watch for file changes and restart the app (optional, useful for development)
+    env: {
+      NODE_ENV: 'production',
+      PORT: 6010
+    },
+    env_production : {
+      NODE_ENV: 'production', // Environment variables for the production environment
+      PORT: 8080
+    }
+  }]
+};
