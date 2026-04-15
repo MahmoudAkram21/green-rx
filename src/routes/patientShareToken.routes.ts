560a86ac5d706c7fcc9e5476a3e58c8fc641b4aa
MahmoudAkram21 <mahmoudakram422@gmail.com>
2026-04-15 17:48:02 +0200

    Update patientShareToken routes to allow pharmacists to redeem share tokens, enhancing access control for token redemption.

--- src/routes/patientShareToken.routes.ts
+++ src/routes/patientShareToken.routes.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 router.use(authenticate);
 
 router.post('/generate', authorize([UserRole.Patient]), generateShareToken);
-router.post('/redeem', authorize([UserRole.Doctor]), redeemShareToken);
+router.post('/redeem', authorize([UserRole.Doctor , UserRole.Pharmacist]), redeemShareToken);
 router.post('/redeem-pharmacist', authorize([UserRole.Pharmacist]), redeemShareTokenAsPharmacist);
 
 export default router;
